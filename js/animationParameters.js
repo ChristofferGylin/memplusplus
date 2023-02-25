0e885618b779fa86bb4c4b979663393d55822e16
Christoffer <christoffer@christoffergylin.com>
2023-02-25 19:20:33 +0100

    Made question mark size dynamic

--- js/animationParameters.js
+++ js/animationParameters.js
@@ -2,10 +2,18 @@ const animationParameters = {
     on: true,
     minTime: 6,
     maxTime: 26,
-    minHeight: 70,
-    maxHeight: 1440,
+    minHeight: window.innerHeight / 20,
+    maxHeight: window.innerHeight,
     nextDirection: 0,
-    highest: 0
 
 }
 export default animationParameters;
+
+window.addEventListener('resize', () => {
+
+    animationParameters.maxHeight = window.innerHeight;
+    animationParameters.minHeight = window.innerHeight / 20;
+
+    console.log(animationParameters.maxHeight);
+
+})
